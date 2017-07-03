0725626d3c1ae84a32809c0fe87d9264f7157aff
Blessing Pascal <pascal.blessing@gmx.at>
2017-07-03 17:02:15 +0200

    Added tests on whether both imports work and whether two equal imports match

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 import Foo from './foo';
 import Foo2 from './Foo';
+import Foo3 from './foo';
+
+console.log(Foo);
+console.log(Foo2);
 
 console.log('Foo === Foo2: ' + (Foo === Foo2));
+console.log('Foo === Foo3: ' + (Foo === Foo3));
+
 
 console.log('new Foo() instanceof Foo2: ' + (new Foo() instanceof Foo2));
 console.log('new Foo2() instanceof Foo: ' + (new Foo2() instanceof Foo));
